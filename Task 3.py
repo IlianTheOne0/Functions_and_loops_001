a8ac26ca24ceb4580ee357453106a3577e43e49f
Ilian Shchepinskyi <iliantheone@gmail.com>
2024-10-25 22:26:45 +0300

    Changed Task 3

--- Task 3.py
+++ Task 3.py
@@ -2,9 +2,9 @@ import func
 
 if __name__ == '__main__':
     try:
-        # a = int(input('Enter the first number: '))
-        # b = int(input('Enter the second number: '))
-        a, b = 3, 5
+        a = int(input('Enter the first number: '))
+        b = int(input('Enter the second number: '))
+
         a, b = func.is_the_biggest_check(a, b)
 
         func.table_of_multiplication(a, b)
