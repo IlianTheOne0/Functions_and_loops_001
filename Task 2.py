0e9ae87cb0aa5567fb454223cbce198ff6966bf4
Ilian Shchepinskyi <iliantheone@gmail.com>
2024-10-25 21:17:09 +0300

    Completed Task 2

---
+++ Task 2.py
@@ -0,0 +1,7 @@
+import func
+
+if __name__ == '__main__':
+    try:
+        func.table_of_multiplication()
+    except Exception as e:
+        print(f'Error: {e}')
--- func.py
+++ func.py
@@ -1,5 +1,21 @@
-def main():
-    try:
-        pass
-    except Exception as e:
-        print(f'Error: {e}')
+def table_of_multiplication():
+    a = 1
+    b = 10
+    first_space = 1
+    second_space = 1
+
+    for i in range(a, b + 1):
+        for j in range(a, b + 1):
+            if i * j < 10:
+                first_space = second_space = 5
+            elif 100 > i * j >= 10 and j == 10:
+                first_space, second_space = 5, 5
+            elif 100 > i * j >= 10 > i:
+                first_space, second_space = 5, 4
+            elif 100 > i * j >= 10:
+                first_space = second_space = 4
+
+
+            temp = '|' + ' ' * first_space + f'{i}*{j}={i * j}' + ' ' * second_space + '|'
+            print(temp, end='')
+        print()
