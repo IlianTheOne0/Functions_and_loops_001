1dd4491d10beb8ecf7c0a478aca387de66733c38
Ilian Shchepinskyi <iliantheone@gmail.com>
2024-10-26 13:12:31 +0300

    Deleted previous unit tests and added new

--- Task 1.py
+++ Task 1.py
@@ -2,11 +2,6 @@ import func
 
 if __name__ == '__main__':
     try:
-        a = int(input('Enter the first number: '))
-        b = int(input('Enter the second number: '))
-
-        a, b = func.is_the_biggest_check(a, b)
-
-        print(f'Prime numbers from {a} to {b}: {func.is_prime_number(a, b)}')
+        func.main()
     except Exception as e:
         print(f'Error: {e}')
--- func.py
+++ func.py
@@ -1,5 +1,14 @@
 # https://stackoverflow.com/questions/5811151/why-do-we-check-up-to-the-square-root-of-a-number-to-determine-if-the-number-is
 
+
+def main():
+    a = int(input('Enter the first number: '))
+    b = int(input('Enter the second number: '))
+
+    a, b = is_the_biggest_check(a, b)
+
+    print(f'Prime numbers from {a} to {b}: {is_prime_number(a, b)}')
+
 def is_the_biggest_check(a, b):
     a, b = (b, a) if a > b else (a, b)
     return a, b
